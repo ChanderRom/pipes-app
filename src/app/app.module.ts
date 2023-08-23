01707c6c474eb529c292cf4b7cbd5623266e14f1
ChaanChanChan <alexandertakepics@gmail.com>
2023-08-23 13:09:02 +0200

    basic and number page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+
+import localeEs from '@angular/common/locales/es'
+import localeFrCA from '@angular/common/locales/fr-CA'
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(localeEs)
+registerLocaleData(localeFrCA)
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SharedModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [
+        {
+            provide: LOCALE_ID, useValue: 'es'
+        }
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
